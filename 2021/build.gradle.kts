8117767e3608b9f04a9876a9495ac3e0d6ee5a0b
Gohlke, Christian <christian.gohlke@spring-media.de>
2021-11-30 22:12:40 +0100

    chore: project AOC2021 initial

---
+++ 2021/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    application
+    kotlin("jvm") version "1.6.0"
+}
+
+application {
+    mainClass.set("util.Runner")
+}
+
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.reflections", "reflections", "0.9.12")
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.8.2")
+    testImplementation("org.junit.jupiter", "junit-jupiter-engine", "5.8.2")
+    testImplementation("org.hamcrest", "hamcrest", "2.2")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
